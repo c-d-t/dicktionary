2537964f1a262cabb782e90584c2a8a279e93849
Chris T <ceasedinsanity.ct@gmail.com>
2019-10-26 20:13:04 -0300

    refactored client javascript for easier scaling in the future

--- client/menu/menu.js
+++ client/menu/menu.js
@@ -1,50 +1,111 @@
-let socket = io()
-
-
-// event listers
+// init
 $(document).ready(function() {
-    $("#room-to-menu-button").click(function(e) { 
-        e.preventDefault()
-        slideRight()
-    })
+    let IO = {
 
-    // join room
+        init : function() {
+            IO.socket = io()
+        },
+
+        bindEvents : function() {
+            IO.socket.on('server:connected', IO.onConnected)
+        },
+
+        onConnected : function() {
+
+        }
+    }
+
+    let App =  {
+
+        roomID : '',
+
+        socketID : '',
+
+        init : function() {
+            this.cacheElements()
+            this.initDisplay()
+        },
+
+        cacheElements : function() {
+            App.$doc = $(document)
+            App.$gamebox = $("#gamebox")
+            App.$menu = $("#menu").html()
+            App.$game = $("game").html()
+        },
+
+        initDisplay : function() {
+            App.$gamebox.html(App.$menu)
+        }
+
+    }
+
+    IO.init()
+    App.init()
+})
+
+    /* OLD
+    
+    // dom elements
+    const $main =  $('#main')
+    const $nickname = $('#nickname')
+    const $room = $('#room')
+
+    // menu transistions
+    $("#nickname-to-menu-button").click(function(e) { 
+        e.preventDefault()
+        slideRight($nickname, $main)
+    })
+    $("#room-to-nickname-button").click(function(e) { 
+        e.preventDefault()
+        slideRight($room, $nickname)
+    })
     $("#join-room-button").click(function(e) { 
         e.preventDefault()
-        slideLeft()
+        slideLeft($main, $nickname)
     })
-
-    // make room
     $("#make-room-button").click(function(e) { 
         e.preventDefault()
-        makeNewRoom()
-        slideLeft()
+        slideLeft($main, $nickname)
+    })
+    $("#choose-nickname-button").click(function(e) { 
+        e.preventDefault()
+        slideLeft($nickname, $room)
+    })
+
+    // socket listeners
+    socket.on('room:new', data => {
+        if(data.error) {
+            console.log(data.error)
+        } else {
+            slideLeft()
+        }
     })
 })
 
 const makeNewRoom = () => {
     let room = $("#make-room-input").val()
-    socket.emit('room:new', {room})
+    socket.emit('room:new', { roomID: room })
 }
 
 // slide animations
-const slideRight  = () => {
-    $("#room").addClass('slide-right-out')
+const slideRight  = ($current, $to) => {
+    $current.addClass('slide-right-out')
     setTimeout(() => {
-        $('#room').removeClass('current-menu slide-right-out')
-    }, 400)
-    $("#main").addClass('current-menu slide-right-in')
+        $current.removeClass('current-menu slide-right-out')
+    }, 250)
+    $to.addClass('current-menu slide-right-in')
     setTimeout(() => {
-        $('#main').removeClass('slide-right-in')
-    }, 400)
+        $to.removeClass('slide-right-in')
+    }, 250)
 }
-const slideLeft = () => {
-    $("#main").addClass('slide-left-out')
+const slideLeft = ($current, $to) => {
+    $current.addClass('slide-left-out')
     setTimeout(() => {
-        $('#main').removeClass('current-menu slide-left-out')
+        $current.removeClass('current-menu slide-left-out')
     }, 400)
-    $("#room").addClass('current-menu slide-left-in')
+    $to.addClass('current-menu slide-left-in')
     setTimeout(() => {
-        $('#room').removeClass('slide-left-in')
+        $to.removeClass('slide-left-in')
     }, 400)
-}
+
+    */
--- server/game-manager.js
+++ server/game-manager.js
@@ -1,19 +1,22 @@
 class GameManager {
-    constructor(io, socket)  {
+    constructor(io, socket) {
         this.io = io // server socket
         this.gameSocket = socket // client socket
+
+        this.currentRooms = []
 
         this.init()
     }
 
     init() {
-        console.log(`${this.gameSocket.id} joined`)
+        this.gameSocket.emit('server:connected', {sessionID: this.gameSocket.id})
 
         this.gameSocket.on('room:new', this.newRoom)
     }
 
-    newRoom() {
-        console.log(this.id)
+    newRoom(data) {
+        let rooms = gameSocket.manager.rooms['/' + data.roomID]
+        this.emit('room:new', {error: null, roomID: 0})
     }
 }
 
